b63f4175199510245c3f80fd2b6eaa2d6294d4e4
Kaibot11 <157419978+kaibot111@users.noreply.github.com>
2026-01-06 14:55:23 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const rooms = {};
+
+io.on('connection', (socket) => {
+    socket.on('joinRoom', (roomCode) => {
+        socket.join(roomCode);
+        if (!rooms[roomCode]) rooms[roomCode] = [];
+        rooms[roomCode].push(socket.id);
+        io.to(roomCode).emit('playerUpdate', rooms[roomCode]);
+    });
+
+    socket.on('updatePos', (data) => {
+        socket.to(data.room).emit('opponentMove', { id: socket.id, pos: data.pos });
+    });
+});
+
+server.listen(process.env.PORT || 3000);
