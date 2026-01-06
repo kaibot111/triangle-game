f19ce8c5974341ca13b5449c5a4c5d6d20d71992
Kaibot11 <157419978+kaibot111@users.noreply.github.com>
2026-01-06 15:06:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -24,4 +24,5 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
